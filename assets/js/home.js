25ee3333bf11b23ec4804270fc3a00fe3dd6f9fd
LunarTiger <lunartiger@live.com>
2020-04-23 19:02:58 -0400

    :tada:

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,6 +1,7 @@
 var place = [
 	"You can contact me at <a href='tel:+15406926899' id='phone'>1-540-692-6899</a> or through my <a href='/contact' id='contact'>contact page</a>.",
 	"I'm home at <a href='https://www.google.com/maps/place/293+Babbs+Mountain+Rd,+Winchester,+VA+22603/@39.2744651,-78.1799907,17z/data=!3m1!4b1!4m5!3m4!1s0x89b5f115682b0d49:0xa79fd3617adf6fc!8m2!3d39.274461!4d-78.177802' id='address' target='_blank'>293 Babbs Mountain Rd. Winchester, VA 22603</a>.",
+	"I'm not home.",
 	"I'm out with my dad, Lanny. He can be contacted at <a href='tel:+15403279023'>1-540-327-9023</a>.",
 	"I'm out with my mom, Shelva. She can be contacted at <a href='tel:+13048204338'>1-304-820-4338</a>."
 ];
@@ -18,11 +19,14 @@ try{
 			if(messageVal=="home"){
 				document.getElementById('more-info').innerHTML = place[1]+" "+place[0];
 			}
-			else if(messageVal=="awayd"){
+			else if(messageVal=="away"){
 				document.getElementById('more-info').innerHTML = place[2]+" "+place[0];
 			}
-			else if(messageVal=="awaym"){
+			else if(messageVal=="dad"){
 				document.getElementById('more-info').innerHTML = place[3]+" "+place[0];
+			}
+			else if(messageVal=="mom"){
+				document.getElementById('more-info').innerHTML = place[4]+" "+place[0];
 			}
 			else{
 				document.getElementById('more-info').innerHTML = messageVal+" "+place[0];
