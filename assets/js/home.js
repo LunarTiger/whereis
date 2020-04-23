022bc1883bf7c62fabb4e8771b6585daa0905173
LunarTiger <lunartiger@live.com>
2020-04-23 12:32:23 -0400

    :tada:

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,7 @@
+var htmldefault = "You can contact me at <a href='tel:+15406926899' id='phone'>1-540-692-6899</a> or through my <a href='/contact' id='contact' target='_blank'>contact page</a>.";
+var htmlhome = "I'm home at <a href='https://www.google.com/maps/place/293+Babbs+Mountain+Rd,+Winchester,+VA+22603/@39.2744651,-78.1799907,17z/data=!3m1!4b1!4m5!3m4!1s0x89b5f115682b0d49:0xa79fd3617adf6fc!8m2!3d39.274461!4d-78.177802' id='address' target='_blank'>293 Babbs Mountain Rd. Winchester, VA 22603</a>.";
+var htmlawayd = "I'm out with my dad, Lanny. He can be contacted at <a href='tel:+15403279023'>1-540-327-9023</a>.";
+var htmlawaym = "I'm out with my mom, Shelva. She can be contacted at <a href='tel:+13048204338'>1-304-820-4338</a>.";
 try{
 	var config = {
 		databaseURL: "https://lunar-home.firebaseio.com/",
@@ -10,23 +14,23 @@ try{
 		var messageVal = snapshot.val();
 		if(messageVal){
 			if(messageVal=="home"){
-				document.getElementById('more-info').innerHTML = "I'm home at <a href='https://www.google.com/maps/place/293+Babbs+Mountain+Rd,+Winchester,+VA+22603/@39.2744651,-78.1799907,17z/data=!3m1!4b1!4m5!3m4!1s0x89b5f115682b0d49:0xa79fd3617adf6fc!8m2!3d39.274461!4d-78.177802' id='address' target='_blank'>293 Babbs Mountain Rd. Winchester, VA 22603</a>. You can contact me at <a href='tel:+15406926899' id='phone'>1-540-692-6899</a> or through my <a href='/contact' id='contact' target='_blank'>contact page</a>.";
+				document.getElementById('more-info').innerHTML = htmlhome+"&nbsp;"+htmldefault;
 			}
 			else if(messageVal=="awayd"){
-				document.getElementById('more-info').innerHTML = "I'm out with my dad, Lanny. He can be contacted at <a href='tel:+15403279023'>1-540-327-9023</a>. You can contact me at <a href='tel:+15406926899' id='phone'>1-540-692-6899</a> or through my <a href='/contact' id='contact' target='_blank'>contact page</a>.";
+				document.getElementById('more-info').innerHTML = htmlawayd+"&nbsp;"+htmldefault;
 			}
 			else if(messageVal=="awaym"){
-				document.getElementById('more-info').innerHTML = "I'm out with my mom, Shelva. She can try contacted at <a href='tel:+13048204338'>1-304-820-4338</a>. You can contact me at <a href='tel:+15406926899' id='phone'>1-540-692-6899</a> or through my <a href='/contact' id='contact' target='_blank'>contact page</a>.";
+				document.getElementById('more-info').innerHTML = htmlawaym+"&nbsp;"+htmldefault;
 			}
 			else{
-				document.getElementById('more-info').innerHTML = messageVal;
+				document.getElementById('more-info').innerHTML = messageVal+"&nbsp;"+htmldefault;
 			}
 		}
 		if(!messageVal){
-			document.getElementById('more-info').innerHTML = "No clue.";
+			document.getElementById('more-info').innerHTML = "No clue. "+htmldefault;
 		}
 	}));
 }
 catch{
-	document.getElementById('more-info').innerHTML = "The script crashed.";
+	document.getElementById('more-info').innerHTML = "The script crashed. "+htmldefault;
 }
